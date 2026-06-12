2e337982a6be03ba1f6459b10f166ebfd3da5c7f
congoimmob-art <congoimmob@gmail.com>
2026-06-13 00:57:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB =
+require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Bienvenue sur l'API CongoImmo"
+    message:
+    "API CongoImmo opérationnelle"
   });
 });
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+const PORT =
+process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
+  console.log(
+    `Serveur démarré sur ${PORT}`
+  );
 });
