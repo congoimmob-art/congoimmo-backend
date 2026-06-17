69038c202c860da71af8446135074fd8ebe0018e
congoimmob-art <congoimmob@gmail.com>
2026-06-17 01:16:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
-});app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
+});
 }); // <-- L'accolade et la parenthèse manquantes ont été ajoutées ici !
 });
